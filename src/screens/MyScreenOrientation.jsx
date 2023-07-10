7fe98889d82b785f9d09a3fce0bd7dedcb2f8411
Henrique <claudineigaldino12@gmail.com>
2023-07-10 15:54:13 -0300

    10/07

--- src/screens/MyScreenOrientation.jsx
+++ src/screens/MyScreenOrientation.jsx
@@ -1,111 +1,103 @@
 import * as ScreenOrientation from 'expo-screen-orientation';
 import React, { useState } from 'react';
-import { View, Button, ScrollView } from 'react-native';
+
+import { View, Button, ScrollView, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 10
+      },
+      center:{
+        flex: 1,
+        gap: 20,
+        padding: 20,
+        alignSelf: 'center',
+      },
+})
+
 
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function MyScreenOrientation() {
-    const [cor, setCor] = useState("#000000");
+function MyScreenOrientation(){
+
+    const [getBackgroundColor, setBackgroundColor] = useState('#fff');
 
     async function defaultOrientation(type) {
-        if (type === 'ALL') {
+        if(type === 'ALL'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.ALL
             );
-            setCor('#36fa00')
-        } else if (type === 'PORTRAIT') {
+            setBackgroundColor('rgba(0, 255, 0, .5)');
+        }else if(type === 'PORTRAIT'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.PORTRAIT
             );
-            setCor('#36fa00')
-        } else if (type === 'PORTRAIT_UP') {
+            setBackgroundColor('rgba(0, 155, 0, .5)');
+        }else if(type === 'PORTRAIT_UP'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.PORTRAIT_UP
             );
-            setCor('#36fa00')
-        } else if (type === 'PORTRAIT_DOWN') {
+            setBackgroundColor('rgba(0, 55, 0, .5)');
+        }else if(type === 'PORTRAIT_DOWN'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.PORTRAIT_DOWN
             );
-            setCor('#36fa00')
-        } else if (type === 'LANDSCAPE') {
+            setBackgroundColor('rgba(0, 100, 0, .5)');
+        }else if(type === 'LANDSCAPE'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.LANDSCAPE
             );
-            setCor('#36fa00')
-        } else if (type === 'LANDSCAPE_LEFT') {
+            setBackgroundColor('rgba(55, 55, 0, .5)');
+        }else if(type === 'LANDSCAPE_LEFT'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.LANDSCAPE_LEFT
             );
-            setCor('#36fa00')
-        } else if (type === 'LANDSCAPE_RIGHT') {
+            setBackgroundColor('rgba(100, 155, 0, .5)');
+        }else if(type === 'LANDSCAPE_RIGHT'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT
             );
-            setCor('#36fa00')
-        } else if (type === 'OTHER') {
+            setBackgroundColor('rgba(120, 90, 0, .5)');
+        }else if(type === 'OTHER'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.OTHER
             );
-            setCor('#36fa00')
-        } else if (type === 'UNKNOWN') {
+            setBackgroundColor('rgba(190, 55, 100, .5)');
+        }else if(type === 'UNKNOWN'){
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.UNKNOWN
             );
-            setCor('#36fa00')
-        } else {
+            setBackgroundColor('rgba(255, 100, 10, .5)');
+        }else{
             await ScreenOrientation.lockAsync(
                 ScreenOrientation.OrientationLock.DEFAULT
             );
+            setBackgroundColor('rgba(255, 255, 255, .5)');
         };
     };
 
     return (
-        <ScrollView >
-            <View style={[{ backgroundColor: cor }]}>
+        <ScrollView style={[styles.container, {backgroundColor: `${getBackgroundColor}`}]}>
+            <View style={styles.container}>
                 <Header title='Orientação de Tela' />
-                <View>
-                    <Button style={{ margin: 10 }} title='DEFAULT' onPress={() => defaultOrientation()} />
-                </View>
-
-                <View>
-                    <Button title='ALL' onPress={() => defaultOrientation('ALL')} />
-                </View>
-
-                <View>
-                    <Button title='PORTRAIT' onPress={() => defaultOrientation('PORTRAIT')} />
-                </View>
-
-                <View>
-                    <Button title='PORTRAIT_UP' onPress={() => defaultOrientation('PORTRAIT_UP')} />
-                </View>
-
-                <View>
-                    <Button title='PORTRAIT_DOWN' onPress={() => defaultOrientation('PORTRAIT_DOWN')} />
-                </View>
-                <Button title='LANDSCAPE' onPress={() => defaultOrientation('LANDSCAPE')} />
-
-                <View>
-                    <Button title='LANDSCAPE_LEFT' onPress={() => defaultOrientation('LANDSCAPE_LEFT')} />
-                </View>
-
-                <View>
-                    <Button title='LANDSCAPE_RIGHT' onPress={() => defaultOrientation('LANDSCAPE_RIGHT')} />
-                </View>
-
-                <View>
-                    <Button title='OTHER' onPress={() => defaultOrientation('OTHER')} />
-                </View>
-
-                <View>
-                    <Button title='UNKNOWN' onPress={() => defaultOrientation('UNKNOWN')} />
-                </View>
-                
-            <Footer text='Sair' />
-        </View>
-        </ScrollView >
+                    <View style={styles.center}>
+                        <Button title='DEFAULT' onPress={() => defaultOrientation()} />
+                        <Button title='ALL' onPress={() => defaultOrientation('ALL')} />
+                        <Button title='PORTRAIT' onPress={() => defaultOrientation('PORTRAIT')} />
+                        <Button title='PORTRAIT_UP' onPress={() => defaultOrientation('PORTRAIT_UP')} />
+                        <Button title='PORTRAIT_DOWN' onPress={() => defaultOrientation('PORTRAIT_DOWN')} />
+                        <Button title='LANDSCAPE' onPress={() => defaultOrientation('LANDSCAPE')} />
+                        <Button title='LANDSCAPE_LEFT' onPress={() => defaultOrientation('LANDSCAPE_LEFT')} />
+                        <Button title='LANDSCAPE_RIGHT' onPress={() => defaultOrientation('LANDSCAPE_RIGHT')} />
+                        <Button title='OTHER' onPress={() => defaultOrientation('OTHER')} />
+                        <Button title='UNKNOWN' onPress={() => defaultOrientation('UNKNOWN')} />
+                    </View>
+                <Footer text='Sair' />
+            </View>
+        </ScrollView>
     )
 };
 
